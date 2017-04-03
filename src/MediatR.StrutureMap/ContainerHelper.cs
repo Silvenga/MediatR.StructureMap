21ad13eae5597bd88b3f272da633362ca35be857
Mark Lopez <m@silvenga.com>
2017-04-02 20:38:44 -0500

    Basic prototype.

---
+++ src/MediatR.StrutureMap/ContainerHelper.cs
@@ -0,0 +1,13 @@
+﻿using StructureMap;
+
+namespace MediatR.StrutureMap
+{
+    public static class ContainerHelper
+    {
+        public static IContainer UseMediatR(this IContainer container)
+        {
+            container.Configure(configure => configure.AddRegistry<MediatRRegistry>());
+            return container;
+        }
+    }
+}
---
+++ src/MediatR.StrutureMap/MediatRRegistry.cs
@@ -0,0 +1,19 @@
+﻿using StructureMap;
+
+namespace MediatR.StrutureMap
+{
+    public class MediatRRegistry : Registry
+    {
+        public MediatRRegistry()
+        {
+            Scan(scanner =>
+            {
+                scanner.AssemblyContainingType<IMediator>();
+                scanner.WithDefaultConventions();
+            });
+
+            For<SingleInstanceFactory>().Use<SingleInstanceFactory>(ctx => t => ctx.GetInstance(t));
+            For<MultiInstanceFactory>().Use<MultiInstanceFactory>(ctx => t => ctx.GetAllInstances(t));
+        }
+    }
+}
