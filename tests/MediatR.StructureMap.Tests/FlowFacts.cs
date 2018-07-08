a5ad7075620d7aa202e20fa1ea918a933edcc2a4
Mark Lopez <m@silvenga.com>
2018-07-08 11:28:13 -0500

    Added test coverage.

---
+++ tests/MediatR.StructureMap.Tests/FlowFacts.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+using AutoFixture;
+
+using FluentAssertions;
+
+using StructureMap;
+
+using Xunit;
+
+namespace MediatR.StructureMap.Tests
+{
+    public class FlowFacts
+    {
+        private static readonly Fixture Autofixture = new Fixture();
+
+        private readonly Container _container;
+
+        public FlowFacts()
+        {
+            _container = new Container();
+            _container.UseMediatR();
+            _container.Configure(config => config.For<IRequestHandler<QueryFixture.Query, QueryFixture.Result>>().Use<QueryFixtureHandler>());
+        }
+
+        [Fact]
+        public async Task FlowWorks()
+        {
+            var mediator = _container.GetInstance<IMediator>();
+            var input = Autofixture.Create<QueryFixture.Query>();
+
+            // Act
+            var result = await mediator.Send(input);
+
+            // Assert
+            result.Output.Should().Be(input.Input);
+        }
+
+        public static class QueryFixture
+        {
+            public class Query : IRequest<Result>
+            {
+                public Guid Input { get; set; }
+            }
+
+            public class Result
+            {
+                public Guid Output { get; set; }
+            }
+        }
+
+        public class QueryFixtureHandler : IRequestHandler<QueryFixture.Query, QueryFixture.Result>
+        {
+            public Task<QueryFixture.Result> Handle(QueryFixture.Query message, CancellationToken cancellationToken)
+            {
+                return Task.FromResult(new QueryFixture.Result {Output = message.Input});
+            }
+        }
+    }
+}
